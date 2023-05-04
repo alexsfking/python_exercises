817794418bac0c3698a7c74dc8160c441d42647f
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-05-04 23:13:17 +1000

    added a chat-gpt comment

--- hackerrank/exercises/tree_preorder_traversal.py
+++ hackerrank/exercises/tree_preorder_traversal.py
@@ -56,6 +56,37 @@ self.right (the right child of the node)
 self.info (the value of the node)
 """
 
+"""
+***Chat-GPT***
+This code defines two classes, `Node` and `BinarySearchTree`. The `Node` class
+represents a single node in a binary tree, and the `BinarySearchTree` class
+represents the entire binary search tree. 
+
+The `preOrder` function takes a pointer to the root node of a binary tree as its
+input and prints the values in the tree's preorder traversal as a single line of
+space-separated values.
+
+The preorder traversal of a binary tree visits the root node first, followed by
+the left subtree, and then the right subtree. The `preOrder` function uses a
+stack to keep track of the nodes that still need to be visited. It initializes
+the stack with the root node and then enters a loop.
+
+In each iteration of the loop, the function pops the top node from the stack,
+appends its value to an output list, and then checks if it has a right child or
+a left child. If it has a right child, the right child is added to the stack,
+and if it has a left child, the left child is added to the stack. 
+
+The loop continues until the stack is empty, which means all the nodes in the
+tree have been visited. Finally, the function joins the output list using a
+space separator and prints it to the console.
+
+The `BinarySearchTree` class has a `create` method that takes a value as input
+and adds it to the binary search tree. If the root node is None, the new node
+becomes the root node. If the value is less than the current node's value, it
+moves to the left subtree. If the value is greater than the current node's
+value, it moves to the right subtree. The `create` method continues until it
+finds an empty spot to insert the new node.
+"""
 
 def preOrder(root):
     #Write your code here
