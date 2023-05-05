84c17b028900750ddd4ea6e604d0a500456b29f7
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-05-05 23:25:40 +1000

    string.printable

---
+++ hackerrank/exercises/tree_huffman_decoding.py
@@ -0,0 +1,150 @@
+import queue as Queue
+
+cntr = 0
+
+class Node:
+    def __init__(self, freq, data):
+        self.freq = freq
+        self.data = data
+        self.left = None
+        self.right = None
+        global cntr
+        self._count = cntr
+        cntr = cntr + 1
+        
+    def __lt__(self, other):
+        if self.freq != other.freq:
+            return self.freq < other.freq
+        return self._count < other._count
+
+def huffman_hidden():#builds the tree and returns root
+    q = Queue.PriorityQueue()
+
+    
+    for key in freq:
+        q.put((freq[key], key, Node(freq[key], key) ))
+    
+    while q.qsize() != 1:
+        a = q.get()
+        b = q.get()
+        obj = Node(a[0] + b[0], '\0' )
+        obj.left = a[2]
+        obj.right = b[2]
+        q.put((obj.freq, obj.data, obj ))
+        
+    root = q.get()
+    root = root[2]#contains root object
+    return root
+
+def dfs_hidden(obj, already):
+    if(obj == None):
+        return
+    elif(obj.data != '\0'):
+        code_hidden[obj.data] = already
+        
+    dfs_hidden(obj.right, already + "1")
+    dfs_hidden(obj.left, already + "0")
+
+"""class Node:
+    def __init__(self, freq,data):
+        self.freq= freq
+        self.data=data
+        self.left = None
+        self.right = None
+"""        
+
+"""
+Huffman coding assigns variable length codewords to fixed length input
+characters based on their frequencies. More frequent characters are assigned
+shorter codewords and less frequent characters are assigned longer codewords.
+All edges along the path to a character contain a code digit. If they are on the
+left side of the tree, they will be a 0 (zero). If on the right, they'll be a 1
+(one). Only the leaves will contain a letter and its frequency count. All other
+nodes will contain a null instead of a character, and the count of the frequency
+of all of it and its descendant characters.
+
+Example 
+
+A - 0
+B - 111
+C - 1100
+D - 1101
+R - 10
+
+'0'    : 'A'
+'111'  : 'B'
+'1100' : 'C'
+'1101' : 'D'
+'10'   : 'R'
+
+A B    R  A C     A D     A B    R  A
+0 111 10 0 1100 0 1101 0 111 10 0
+or
+01111001100011010111100
+"""
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import string
+
+def decodeHuff(root, s):
+    #Enter Your Code Here
+    #generate a dict of huffman codes
+    stack=[root]
+    character_set=set(string.printable)
+    root.huff=""
+    huff_dict=dict()
+    while(stack):
+        node=stack.pop()
+        if(node.data in character_set):
+            huff_dict[node.huff]=node.data
+        if(node.left):
+            stack.append(node.left)
+            node.left.huff=node.huff+"0"
+        if(node.right):
+            stack.append(node.right)
+            node.right.huff=node.huff+"1"
+    
+    #decode string into the input string
+    start_index=0
+    end_index=1
+    out=[]
+    while(end_index<len(s)):
+        if(s[start_index:end_index] in huff_dict):
+            out.append(huff_dict[s[start_index:end_index]])
+            start_index=end_index
+            end_index=start_index+1
+        else:
+            end_index+=1
+    if(start_index<len(s) and s[start_index:] in huff_dict):
+        out.append(huff_dict[s[start_index:]])
+    print("".join(out))
+        
+
+ip = input()
+freq = {}#maps each character to its frequency
+
+cntr = 0
+
+for ch in ip:
+    if(freq.get(ch) == None):
+        freq[ch] = 1
+    else:
+        freq[ch]+=1
+
+root = huffman_hidden()#contains root of huffman tree
+
+code_hidden = {}#contains code for each object
+
+dfs_hidden(root, "")
+
+if len(code_hidden) == 1:#if there is only one character in the i/p
+    for key in code_hidden:
+        code_hidden[key] = "0"
+
+toBeDecoded = ""
+
+for ch in ip:
+   toBeDecoded += code_hidden[ch]
+
+decodeHuff(root, toBeDecoded)
