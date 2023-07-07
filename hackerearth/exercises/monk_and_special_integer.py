63108b72f2b6c17281903d01cec09c88c990607d
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-07-07 22:48:16 +1000

    solved but i think it can be faster

--- hackerearth/exercises/monk_and_special_integer.py
+++ hackerearth/exercises/monk_and_special_integer.py
@@ -8,17 +8,16 @@ n_size, x_max = map(int,input().split())
 array_list:list = list(map(int,input().split()))
 
 start,end,window_sum=0,0,0
-size_set=False
 window_max=len(array_list)
 
 while(end<len(array_list)):
     window_sum+=array_list[end]
     window_size=end-start+1
     if(window_sum>x_max):
-            #reduce the maximum winodw size
+            #reduce the maximum window size
             window_sum=0
             window_max=window_size-1
-            start,end=0,0
+            end=start
             continue
     end+=1
     if window_size == window_max:
