3e48c9ecaeeef8c8b172611619ed06ac994a0ae8
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-07-12 01:01:02 +1000

    solves the problem but exceeds the memory limit

--- hackerearth/exercises/monk_and_prisoner_of_azkaban.py
+++ hackerearth/exercises/monk_and_prisoner_of_azkaban.py
@@ -14,14 +14,14 @@ array_list=list(map(int,input().split()))
 stack=[]
 x_list=[]
 stack.append((array_list[0],0))
-x_list.append('-1')
+x_list.append(-1)
 # build x_list
 for i in range(1,len(array_list)):
     while(stack and stack[-1][0]<=array_list[i]):
         stack.pop()
     if(stack):
-        stack.append((array_list[i],stack[-1][1]+1))
         x_list.append(stack[-1][1]+1)
+        stack.append((array_list[i],i))
     else:
         stack.append((array_list[i],i))
         x_list.append(-1)
@@ -30,19 +30,23 @@ for i in range(1,len(array_list)):
 stack=[]
 y_list=[]
 stack.append((array_list[-1],len(array_list)-1))
-for i in range(len(array_list)-1,-1,-1):
+y_list.append(-1)
+for i in range(len(array_list)-2,-1,-1):
     while(stack and stack[-1][0]<=array_list[i]):
         stack.pop()
     if(stack):
-        stack.append((array_list[i],stack[-1][1]+1))
         y_list.append(stack[-1][1]+1)
+        stack.append((array_list[i],i))
     else:
         stack.append((array_list[i],i))
         y_list.append(-1)
 
 out=[]
-for i in range(len(x_list)):
-    out.append(str(x_list[i]+y_list[i]))
+x_list.reverse()
+while y_list:
+    out.append(str(x_list.pop()+y_list.pop()))
 
+#print(x_list)
+#print(y_list)
 print(" ".join(out))
 
