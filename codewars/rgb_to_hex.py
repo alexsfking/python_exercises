3d09cd03573610ba6847788a84e3a4a79fde35f5
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-08-14 00:11:24 +1000

    updated

--- codewars/rgb_to_hex.py
+++ codewars/rgb_to_hex.py
@@ -1,6 +1,8 @@
-hex_converter = lambda num: format(num, '02X')
+# min 0 max 255
+hex_converter = lambda num: format(max(0, min(num, 255)), '02X') 
 
 def rgb(r, g, b):
     return(''.join(map(hex_converter, (r, g, b))))
 
-rgb(1, 2, 3)
+print(rgb(1, 2, 3))
+print(rgb(-20, 275, 125))
