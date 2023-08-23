17fbedec6c5a7330d9e756ed88c7b10052069c83
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-08-24 00:07:18 +1000

    refactored the code

--- codewars/multiples_of_3_or_5.py
+++ codewars/multiples_of_3_or_5.py
@@ -1,15 +1,15 @@
-
+def calc_sum(number,divisor):
+    quotient=number//divisor
+    sum_divisor = (quotient / 2 * (divisor + quotient * divisor)) if number > divisor-1 else 0
+    return sum_divisor
 
 def solution(number):
     number-=1
     if(number<3):
         return 0
-    three=number//3
-    sum_three=three/2*(3+three*3)
-    five=number//5
-    sum_five = (five / 2 * (5 + five * 5)) if number > 4 else 0
-    three_five=number//15
-    sum_three_five = (three_five / 2) * (15 + three_five * 15) if number > 14 else 0
+    sum_three = calc_sum(number,3)
+    sum_five = calc_sum(number,5)
+    sum_three_five = calc_sum(number,15)
     return sum_three+sum_five-sum_three_five
 
 print(solution(6))
