8a42f248b33ce415dee3ddec7fdf78b567ec7660
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-08-31 00:47:36 +1000

    passed

--- codewars/alphabet_slices.py
+++ codewars/alphabet_slices.py
@@ -1,35 +1,22 @@
 def solution(s:str):
-    stack=[]
-    out=[]
-    increasing,decreasing=False,False
+    stack,out=[],[]
     for i in range(len(s)-1):
-        char_value=ord(s[i])
-        next_char_value=ord(s[i+1])
-        if(char_value==next_char_value+1 and not increasing):
-            decreasing=True
+        if(ord(s[i])+1==ord(s[i+1])):
             stack.append(s[i])
-        elif(char_value==next_char_value-1 and not decreasing):
-            increasing=True
+        elif(stack):
             stack.append(s[i])
-        elif(increasing):
-            stack.append(s[i])
-            increasing=False
-            while(stack):
-                out.append(stack.pop())
-        elif(decreasing):
-            stack.append(s[i])
-            decreasing=False
             while(stack):
                 out.append(stack.pop())
         else:
             out.append(s[i])
-    if(increasing or decreasing):
+    
+    if stack and ord(s[-1]) == ord(stack[-1]) + 1:
         stack.append(s[-1])
-    while(stack):
-        out.append(stack.pop())
-    if(increasing or decreasing):
-        pass
+        while stack:
+            out.append(stack.pop())
     else:
+        while stack:
+            out.append(stack.pop())
         out.append(s[-1])
     return "".join(out)
 
