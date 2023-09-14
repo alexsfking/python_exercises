65322d375f6ccb4f6e62e68e0ee329883f679e83
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-09-14 22:30:19 +1000

    one line

--- codewars/stop_spinning_my_words.py
+++ codewars/stop_spinning_my_words.py
@@ -1,9 +1,2 @@
 def spin_words(sentence):
-    words=sentence.split()
-    out=[]
-    for word in words:
-        if(len(word)>4):
-            out.append(word[::-1])
-        else:
-            out.append(word)
-    return ' '.join(out)
+    return ' '.join([word[::-1] if len(word) > 4 else word for word in sentence.split()])
