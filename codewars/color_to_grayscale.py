6cb339230b7df99942ca9be79e348af95916454d
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-09-20 07:41:29 +1000

    one line

--- codewars/color_to_grayscale.py
+++ codewars/color_to_grayscale.py
@@ -1,8 +1,2 @@
 def rgb_to_grayscale(color:str)->str:
-    r=0.299*int(color[1:3],16)
-    g=0.587*int(color[3:5],16)
-    b=0.114*int(color[5:7],16)
-    y=hex(round(r+g+b))[2:].upper()
-    if(len(y)==1):
-        y = '0' + y
-    return '#' + y * 3
+    return '#' + (hex(round(0.299 * int(color[1:3], 16) + 0.587 * int(color[3:5], 16) + 0.114 * int(color[5:7], 16)))[2:].upper()).zfill(2) * 3
