8a06d517dbf6f81fb6419ee62e9625978890cb5c
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-10-11 00:09:02 +1100

    passed

--- codewars/who_likes_it.py
+++ codewars/who_likes_it.py
@@ -1,7 +1,12 @@
 def likes(names):
-    if(not names):
-        return "no one likes this"
-    elif(len(names)<3):
-        return " and ".join(names) + "like this"
-    elif(len(names)<3):
-    pass
+    match len(names):
+        case 0:
+            return "no one likes this"
+        case 1:
+            return f"{names[0]} likes this"
+        case 2:
+            return f"{names[0]} and {names[1]} like this"
+        case 3:
+            return f"{names[0]}, {names[1]} and {names[2]} like this"
+        case _:
+            return f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
