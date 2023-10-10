be2509280c31435220b2e8af84cd0ae1519b86cc
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-10-10 23:59:32 +1100

    started

---
+++ codewars/who_likes_it.py
@@ -0,0 +1,7 @@
+def likes(names):
+    if(not names):
+        return "no one likes this"
+    elif(len(names)<3):
+        return " and ".join(names) + "like this"
+    elif(len(names)<3):
+    pass
