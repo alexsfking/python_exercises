4f18f5735eba59f4d9ed15d44e26d1632e3f121c
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-11-27 23:58:08 +1100

    working on it

--- codewars/molecule_to_atoms.py
+++ codewars/molecule_to_atoms.py
@@ -1,5 +1,5 @@
 '''
-### kyu 4 ###
+### kyu 5 ###
 For a given chemical formula represented by a string, count the number of atoms
 of each element contained in the molecule and return an object (associative
 array in PHP, Dictionary<string, int> in C#, Map<String,Integer> in Java).
@@ -25,14 +25,102 @@ nitrogen atoms and six oxygen atoms.
 Note that brackets may be round, square or curly and can also be nested. Index
 after the braces is optional.
 '''
+open_bracket_set=set(['[','('])
+
+def contains_brackets(formula:str)->bool:
+    for c in formula:
+        if c in open_bracket_set:
+            return True
+    return False
+
+def contains_numbers(formula:str)->bool:
+    for c in formula:
+        if c.isdigit():
+            return True
+    return False
+
+def remove_brackets(formula:str)->str:
+    bracket = None
+    closing_bracket = None
+    start_index = None
+    multiplier = None
+    for i,char in enumerate(formula):
+        if not bracket:
+            if char in open_bracket_set:
+                bracket = char
+                if bracket == '[':
+                    closing_bracket = ']'
+                else:
+                    closing_bracket = ')'
+                start_index=i
+                break
+    end_index = formula.index(closing_bracket,i)
+    for j in range(end_index+1,len(formula)):
+        if not formula[j].isdigit():
+            multiplier = int(formula[end_index+1:j])
+            break
+    if(not multiplier):
+        multiplier = int(formula[end_index+1:])
+    return formula[:start_index]+formula[start_index+1:end_index]*multiplier+formula[end_index+1+len(str(multiplier)):]
+
+def remove_numbers(formula:str)->str:
+    result = ""
+    i = 0
+    while i < len(formula):
+        if formula[i].isalpha() and i + 1 < len(formula) and formula[i + 1].isdigit():
+            if(formula[i].isupper()):
+                letters=formula[i]
+            else:
+                letters=formula[i-1:i+1]
+            for j in range(i+2,len(formula)):
+                if(not formula[j].isdigit()):
+                    multiplier=int(formula[i+1:j])
+                    break
+            else:
+                multiplier=int(formula[i+1:])
+            result += letters * multiplier
+            i += 1 + len(str(multiplier))  # Skip the letter and the following number
+        else:
+            result += formula[i]
+            i += 1
+
+    return result
 
 def parse_molecule (formula:str)->dict:
+    print(formula)
     atom_count_dict=dict()
-    elements_list=[]
-    for char in formula:
-        if char.isalpha():
-            elements_list.append(char)
+    while(contains_brackets(formula)):
+        formula = remove_brackets(formula)
+    print(formula)
+    while(contains_numbers(formula)):
+        formula = remove_numbers(formula)
+    print(formula)
+    form=list(formula)
+    lower_case_char_list=[]
+    while(len(form)):
+        if(form[-1].isupper()):
+            if(len(lower_case_char_list)):
+                element = form.pop()+lower_case_char_list.pop()
+                if element in atom_count_dict:
+                    atom_count_dict[element] += 1
+                else:
+                    atom_count_dict[element] = 1
+            else:
+                element = form.pop()
+                if element in atom_count_dict:
+                    atom_count_dict[element] += 1
+                else:
+                    atom_count_dict[element] = 1
+        else:
+            lower_case_char_list.append(form.pop())
+    return atom_count_dict
+
+
+print(parse_molecule("C6H12O6"))
+print(parse_molecule("K4[ON(SO3)2]2"))
+print(parse_molecule("Mg(OH)2"))
         
 
-
 # K4[ON(SO3)2]2CO2(SO4)2
+
+
