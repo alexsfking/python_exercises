0b6bf7db7a33ec3663bd657b68013eb3b949582d
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-12-18 23:54:33 +1100

    passed

---
+++ codewars/find_the_unique_number.py
@@ -0,0 +1,28 @@
+'''
+### kyu 6 ###
+Tere is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+
+This is the first kata in series:
+
+Find the unique number (this kata)
+Find the unique string
+Find The Unique
+'''
+
+def find_uniq(arr:list[int | float]) -> int | float:
+    uniq_set = set()
+    not_uniq_set = set()
+    for num in arr:
+        if num not in not_uniq_set:
+            if num in uniq_set:
+                not_uniq_set.add(num)
+                uniq_set.discard(num)
+            else:
+                uniq_set.add(num)
+    return max(uniq_set)
