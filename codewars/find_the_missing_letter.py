f2160b9154790707aef96b034b282016bb1ca908
alexsfking <104585968+alexsfking@users.noreply.github.com>
2024-01-07 00:56:28 +1100

    passed

--- codewars/find_the_missing_letter.py
+++ codewars/find_the_missing_letter.py
@@ -18,12 +18,30 @@ class Missing:
     alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     alphabet_dict = {letter: index for index, letter in enumerate(alphabet)}
 
-    def __init__(self, letters:list) -> None:
-        for letter in letters:
-            
+    def __init__(self, letters:list[str]) -> None:
+        self.letters = letters
 
-    def missing(self):
-        pass
+    def get_missing_letter(self) -> str:
+        last_index = None
+        for letter in self.letters:
+            current_index = Missing.alphabet_dict[letter.upper()]
+            if last_index is not None and current_index - last_index != 1:
+                return self.match_index(current_index - 1, letter)
+            else:
+                last_index = current_index
+
+    def match_index(self, target_index, letter:str) -> str:
+        for k,v in Missing.alphabet_dict.items():
+            if v == target_index:
+                if letter.isupper():
+                    return k
+                else:
+                    return k.lower()
 
 def find_missing_letter(chars:list[str]) -> str:
-    
+    missing =  Missing(chars)
+    return missing.get_missing_letter()
+
+tests =[['a','b','c','d','f'], ['O','Q','R','S'], ['b','d'], ['a', 'c', 'd']]
+for test in tests:
+    print(find_missing_letter(test))
